8059f8f6ab664874ec7a883e9242e1c65e21439f
sunxingbao <sunxingbao@kingsoft.com>
2019-09-16 16:53:09 +0800

    修改redis test文件

--- ksyun/resource_ksyun_redis_instance_test.go
+++ ksyun/resource_ksyun_redis_instance_test.go
@@ -1,17 +1,18 @@
 package ksyun
 
 import (
-	"fmt"
 	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
-	"github.com/pkg/errors"
-	"strings"
 	"testing"
+	"github.com/hashicorp/terraform/terraform"
+	"fmt"
+	"os"
+	"strings"
+	"github.com/pkg/errors"
 )
 
 func TestAccKcs_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
+		PreCheck:     func() {
 			testAccPreCheck(t)
 		},
 		Providers:    testAccProviders,
@@ -68,7 +69,23 @@ func testAccCheckKcsDestroy(s *terraform.State) error {
 
 const testAccKcsConfig = `
 variable "available_zone" {
-  default = "cn-shanghai-3a"
+  default = "cn-beijing-6a"
+}
+resource "ksyun_vpc" "default" {
+  vpc_name   = "ksyun-vpc-tf"
+  cidr_block = "10.7.0.0/21"
+}
+resource "ksyun_subnet" "foo" {
+  subnet_name      = "ksyun-subnet-tf"
+  cidr_block = "10.7.0.0/21"
+  subnet_type = "Reserve"
+  dhcp_ip_from = "10.7.0.2"
+  dhcp_ip_to = "10.7.0.253"
+  vpc_id  = "${ksyun_vpc.default.id}"
+  gateway_ip = "10.7.0.1"
+  dns1 = "198.18.254.41"
+  dns2 = "198.18.254.40"
+  availability_zone = "${var.available_zone}"
 }
 
 variable "protocol" {
@@ -81,8 +98,8 @@ resource "ksyun_redis_instance" "default" {
   mode                  = 2
   capacity              = 1
   net_type              = 2
-  vnet_id               = "bb94ca2d-5865-44b4-a0b4-3c7117185459"
-  vpc_id                = "585c8a05-47f1-4934-a556-1aae96841692"
+  vnet_id               = "${ksyun_subnet.foo.id}"
+  vpc_id                = "${ksyun_vpc.default.id}"
   bill_type             = 5
   duration              = ""
   duration_unit         = ""
@@ -94,8 +111,8 @@ resource "ksyun_redis_instance" "default" {
     "appendonly"                  = "no",
     "appendfsync"                 = "everysec",
     "maxmemory-policy"            = "volatile-lru",
-    "hash-max-ziplist-entries"    = "513",
-    "zset-max-ziplist-entries"    = "129",
+    "hash-max-ziplist-entries"    = "512",
+    "zset-max-ziplist-entries"    = "128",
     "list-max-ziplist-size"       = "-2",
     "hash-max-ziplist-value"      = "64",
     "notify-keyspace-events"      = "",
