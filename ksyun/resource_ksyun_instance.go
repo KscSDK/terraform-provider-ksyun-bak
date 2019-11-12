02f1e1ac933156500382f85c70f832a7c465e7cb
zhuxuanxuan <zhuxuanxuan@zhuxuanuandeMBP.kingsoft.cn>
2019-11-12 15:24:45 +0800

    modify bug

--- ksyun/resource_ksyun_instance.go
+++ ksyun/resource_ksyun_instance.go
@@ -108,7 +108,7 @@ func resourceKsyunInstance() *schema.Resource {
 			},
 			"purchase_time": {
 				Type:     schema.TypeInt,
-				Required: false,
+				Optional: true,
 			},
 			"security_group_id": {
 				Type:     schema.TypeSet,
